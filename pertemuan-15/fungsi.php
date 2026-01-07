9c5c827b1527f72d1883460a2fb1b3a7b12f1d3d
Yassar Manse <2511500013@mahasiswa.atmaluhur.ac.id>
2026-01-07 16:06:27 +0700

    memperbaiki data yg tidak terkirim ke database

--- pertemuan-15/fungsi.php
+++ pertemuan-15/fungsi.php
@@ -1,20 +1,25 @@
 <?php
 require_once 'koneksi.php';
-if (isset($_POST['submit'])) {
-    $nim = $_POST['txtNim'];
-    $nama = $_POST['txtnamalengkap'];
-    $email = $_POST['txttempatlahir'];
-    $pesan = $_POST['txttglahir'];
-    $hobi = $_POST['txthobi'];
-    $pasangan = $_POST['txtpasangan'];
-    $pekerjaan = $_POST['txtpekerjaan'];
-    $ortu = $_POST['txtnamaortu'];
-    $kakak = $_POST['txtnamakakak'];
-    $adik = $_POST['txtnamaadik'];
 
-    $query = "INSERT INTO kontak (txtnim, txtnamalengkap, txttempatlahir, txttglahir, txthobi, txtpasangan, txtpekerjaan, txtnamaortu, txtnamakakak, txtnamaadik) VALUES ('$nim', '$nama', '$email', '$pesan', '$hobi', '$pasangan', '$pekerjaan', '$ortu', '$kakak', '$adik')";
+if (isset($_POST['submit'])) {
+    $nim          = $_POST['txtNim'];
+    $nama         = $_POST['txtnamalengkap'];
+    $tempatlahir  = $_POST['txttempatlahir'];
+    $tglahir      = $_POST['txttglahir'];
+    $hobi         = $_POST['txthobi'];
+    $pasangan     = $_POST['txtpasangan'];
+    $pekerjaan    = $_POST['txtpekerjaan'];
+    $ortu         = $_POST['txtnamaortu'];
+    $kakak        = $_POST['txtnamakakak'];
+    $adik         = $_POST['txtnamaadik'];
+
+    $query = "INSERT INTO kontak 
+    (txtnim, txtnamalengkap, txttempatlahir, txttglahir, txthobi, txtpasangan, txtpekerjaan, txtnamaortu, txtnamakakak, txtnamaadik)
+    VALUES 
+    ('$nim', '$nama', '$tempatlahir', '$tglahir', '$hobi', '$pasangan', '$pekerjaan', '$ortu', '$kakak', '$adik')";
+
     if (mysqli_query($koneksi, $query)) {
-        echo "Data berhasil disimpan.";
+        echo "Data berhasil disimpan";
     } else {
         echo "Error: " . mysqli_error($koneksi);
     }
